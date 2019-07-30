94c9224534bfab35c24abdbeb2784a45803c69ce
Josh Teperman <joshteperman@gmail.com>
2019-07-30 16:13:47 +1000

    added custom validation method start_time_cannot_be_after_end_time and test

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,3 +1,15 @@
 class Timesheet < ApplicationRecord
   validates :date, :start_time, :end_time, presence: true
+  validate :start_time_cannot_be_after_end_time
+  validate :date_cannot_be_in_the_future
+
+  private
+
+  def start_time_cannot_be_after_end_time
+    if start_time.present? && end_time.present?
+      if start_time >= end_time
+        errors.add(:start_time, 'must be before end time')
+      end
+    end
+  end
 end
--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -3,37 +3,21 @@ require 'date'
 # FactoryBot.find_definitions
 
 RSpec.describe Timesheet, type: :model do
-  describe 'Model validations' do
+  describe 'Model validations' do    
     before do
       Timesheet.destroy_all
     end
-    # let(:factory_timesheet) { create(:timesheet) }
 
-    # let(:factory_timesheet) { FactoryBot.create(:timesheet) }
     let(:valid_timesheet) do
-      date = Date.new
-      start_time = Time.parse('9:00')
-      end_time = Time.parse('18:00')
-      Timesheet.new(date: date, start_time: start_time, end_time: end_time)
+      build(:timesheet)
     end
 
-    let(:invalid_timesheet) do
-      start_time = Time.parse('9:00')
-      end_time = Time.parse('18:00')
-      Timesheet.create(start_time: start_time, end_time: end_time)
+    let(:mocks) do
+      attributes_for(:timesheet)
     end
 
     it 'is invalid when intialized without attributes' do
       expect(Timesheet.new).to be_invalid
-    end
-
-    it 'FactoryBot Timesheet is valid' do
-      test_timesheet = build(:timesheet)
-      p test_timesheet
-      p test_timesheet.date
-      p test_timesheet.start_time
-      p test_timesheet.end_time
-      expect(test_timesheet).to be_valid
     end
 
     it 'is valid with valid attributes' do
@@ -41,17 +25,37 @@ RSpec.describe Timesheet, type: :model do
     end
 
     it 'is not valid without a date' do
+      expect(Timesheet.new(
+        start_time: mocks[:start_time],
+        end_time: mocks[:end_time]
+      )).to be_invalid
+    end
+
+    it 'is not valid without a start_time' do
+      expect(Timesheet.new(
+        date: mocks[:date],
+        end_time: mocks[:end_time]
+      )).to be_invalid
+    end
+
+    it 'is not valid without an end_time' do
+      expect(Timesheet.new(
+        date: mocks[:date],
+        start_time: mocks[:start_time]
+      )).to be_invalid
+    end
+
+    it 'is not valid when start_time is after end_time' do
+      invalid_timesheet = build(:timesheet, start_time: Time.zone.parse('19:00'))
       expect(invalid_timesheet).to be_invalid
     end
-    
-    it 'is not valid without a start_time'
-    it 'is not valid without a end_time'
+
+    it 'is not valid when start_time is the same time as end_time' do
+      invalid_timesheet = build(:timesheet, start_time: Time.zone.parse('18:00'))
+      expect(invalid_timesheet).to be_invalid
+    end
+
     it 'is not valid when date is in the future'
     it 'is not valid when times overlap'
-    it 'is not valid when end_time is before start_time'
   end
-
-
-    # test_timesheet = Timesheet.create(Date.new)
-    # expect(test_timesheet).to
 end
