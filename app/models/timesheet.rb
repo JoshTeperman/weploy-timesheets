b1425cb4ab8d8e2dc4375f6d93330eb4ff229508
Josh Teperman <joshteperman@gmail.com>
2019-07-30 22:06:05 +1000

    validation method time_sheet_cannot_overlap and tests

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,15 +1,39 @@
 class Timesheet < ApplicationRecord
   validates :date, :start_time, :end_time, presence: true
+  # validates :amount, :float
   validate :start_time_cannot_be_after_end_time
   validate :date_cannot_be_in_the_future
+  validate :time_sheet_cannot_overlap
 
   private
 
   def start_time_cannot_be_after_end_time
     if start_time.present? && end_time.present?
-      if start_time >= end_time
-        errors.add(:start_time, 'must be before end time')
+      errors.add(:start_time, 'must be before end time') if start_time >= end_time
+    end
+  end
+
+  def date_cannot_be_in_the_future
+    if date.present?
+      errors.add(:date, 'cannot be in the future') if date > Date.current
+    end
+  end
+
+  def time_sheet_cannot_overlap
+    if start_time.present? && end_time.present?
+      Timesheet.all.map do |timesheet|
+        return false if timesheet.id == self.id
+        errors.add(:timesheet, 'cannot overlap') if overlaps?(timesheet)
       end
     end
+  end
+
+  def overlaps?(other)
+    a_start = start_time.seconds_since_midnight
+    a_end = end_time.seconds_since_midnight
+    b_start = other.start_time.seconds_since_midnight
+    b_end = other.end_time.seconds_since_midnight
+
+    (a_start...a_end).overlaps?(b_start...b_end)
   end
 end
