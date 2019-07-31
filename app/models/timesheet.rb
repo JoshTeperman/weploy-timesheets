476a0c55106f8ae34c7693db0cf4e7103fbf229f
Josh Teperman <joshteperman@gmail.com>
2019-07-31 21:53:23 +1000

    fixed flash modals delete button

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -23,7 +23,6 @@ class Timesheet < ApplicationRecord
       Timesheet.all.map do |timesheet|
         p timesheet.id == id
         unless timesheet.id == id
-          # errors.add(:timesheet, 'cannot overlap') if overlaps?(timesheet)
           if overlaps?(timesheet)
             p 'overlaps'
             errors.add(:base, 'Timesheets cannot overlap')
@@ -38,11 +37,17 @@ class Timesheet < ApplicationRecord
   def overlaps?(other)
     p 'checking overlap'
     a_start = start_time.seconds_since_midnight
+    p a_start
     a_end = end_time.seconds_since_midnight
+    p a_end
     b_start = other.start_time.seconds_since_midnight
+    p b_start
     b_end = other.end_time.seconds_since_midnight
+    p b_end
 
+    p (a_start...a_end).overlaps?(b_start...b_end)
     if (a_start...a_end).overlaps?(b_start...b_end)
+      p 'true'
       return true
     end
   end
